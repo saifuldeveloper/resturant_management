918dc5cff7edbde883a3d13f877211e866225d29
saifuldeveloper <mdsaiful77336@gmail.com>
2022-06-10 01:06:01 +0600

    admin panel added

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,46 @@
+<div class="container-scroller">
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+      <a class="sidebar-brand brand-logo" href="admin/index.html"><img src="assets/images/logo.svg" alt="logo" /></a>
+      <a class="sidebar-brand brand-logo-mini" href="admin/index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+    </div>
+    <ul class="nav">
+      <li class="nav-item nav-category">
+        <span class="nav-link">Navigation</span>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="admin/index.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-speedometer"></i>
+          </span>
+          <span class="menu-title">Users</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="admin/pages/forms/basic_elements.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-playlist-play"></i>
+          </span>
+          <span class="menu-title">Foods</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="admin/pages/tables/basic-table.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-table-large"></i>
+          </span>
+          <span class="menu-title">Chafs</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="admin/pages/charts/chartjs.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-chart-bar"></i>
+          </span>
+          <span class="menu-title">Reservation</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+</div>
